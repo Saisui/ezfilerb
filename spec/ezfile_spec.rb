25faacce20347097bbb5c0bf754ccd7299882289
aa <37037844+Saisui@users.noreply.github.com>
2023-11-24 08:38:18 +0800

    ~ single quote

--- spec/ezfile_spec.rb
+++ spec/ezfile_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe Ezfile do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Ezfile::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(true)
   end
 end
